b46611b71d651de98134235f4c472f55039a953d
Fpantoja2001 <121701118+Fpantoja2001@users.noreply.github.com>
2023-05-01 14:06:34 -0400

    feat: add gh pages dependencies

---
+++ src/components/landingpage/index.jsx
@@ -0,0 +1,7 @@
+import './main.scss'
+
+export default function LandingPage(){
+    return (
+        <h1>Landing</h1>
+    )
+}
---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,7 @@
+import './main.scss'
+
+export default function Navbar(){
+    return (
+        <h1>Navbar</h1>
+    )
+}
